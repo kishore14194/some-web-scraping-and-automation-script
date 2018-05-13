c0ae9338d7e12f5b4b7ce79592df9688fd36d37d
kishore <thereal.hunter@gmail.com>
2018-05-14 01:13:28 +0530

    Code Cleanup

--- video_downloader.py
+++ video_downloader.py
@@ -9,7 +9,39 @@ WEBM_FORMAT = 'webm'
 DEFAULT_VERSION = 9
 
 
-class Download:
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="YouTube Url")
+    parser.add_argument("--path", help="Path to store the file")
+    parser.add_argument("operation", help="best - Displays the best version, available_version - List of avaliable "
+                                          "Version, download - Download the video",
+                        choices=["best", "available_version", "download"])
+    parser.add_argument("--version", help="Version to download")
+
+    args = parser.parse_args()
+
+    url = args.url
+    dir_loc = args.path
+
+    is_valid_video = validate_video(url)
+
+    if not is_valid_video:
+        print("-- Entered video is not valid - " + url + " --")
+        sys.exit()
+
+    download = DownloadYoutubeVideo(url)
+    version = args.version
+
+    if args.operation == "best":
+        print(download.best_resolution)
+    if args.operation == "available_version":
+        print(download.available_version)
+    if args.operation == "download":
+        print(download.download_vid(dir_loc, version))
+
+
+class DownloadYoutubeVideo:
+    """ Download youtube video using URL"""
     def __init__(self, url):
         self.video = pafy.new(url)
         self.title = self.video.title.encode('ascii', 'ignore').decode('ascii')
@@ -17,12 +49,14 @@ class Download:
 
     @property
     def best_resolution(self):
+        """ Fetch best available resolution """
         best = self.video.getbest()
         best = format_resolution(best.resolution)
         return "Best Resolution - " + best
 
     @property
     def available_version(self):
+        """ Fetch all the available version """
         res_list = []
         for ind, s in enumerate(self.streams):
             res = format_resolution(s.resolution)
@@ -53,43 +87,11 @@ class Download:
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", help="YouTube Url")
-    parser.add_argument("--path", help="Path to store the file")
-    parser.add_argument("operation", help="best - Displays the best version, available_version - List of avaliable "
-                                          "Version, download - Download the video",
-                        choices=["best", "available_version", "download"])
-    parser.add_argument("--version", help="Version to download")
-
-    args = parser.parse_args()
-
-    url = args.url
-    DIR_LOC = args.path
-
-    is_valid_video = validate_video(url)
-
-    if not is_valid_video:
-        print("-- Entered video is not valid - " + url + " --")
-        sys.exit()
-
-    download = Download(url)
-    version = args.version
-
-    if args.operation == "best":
-        print(download.best_resolution)
-    if args.operation == "available_version":
-        print(download.available_version)
-    if args.operation == "download":
-        print(download.download_vid(DIR_LOC, version))
+    main()
 
 # d = Download("https://www.youtube.com/watch?v=R3GfuzLMPkA")
-# DIR_LOC = "/Users/kishore/Documents/Video/"
-# print(d.best_resolution())
-# print(d.available_version())
-# print(d.download_vid(DIR_LOC))
 
 # url = "https://www.instagram.com/p/BikYx5agUBw/"
-# a = requests.get(url).text
 # insta_link = re.findall('(?<=/)[\w\-\_]+\.mp4', a)
 # insta_link = re.findall("https://instagram[\s\S]*?.mp4", a)
-# insta_link = list(set(insta_link))
+
